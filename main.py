bfc5a017195a9c6a9e23882788b3fbc15ed04e46
Leonardo Sextare <leonardosextare@gmail.com>
2025-04-06 17:54:44 -0300

    altera main

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-from dotenv import load_dotenv
-from tests.ExecutarTestes import executar_testes
-
-print("Carregando as variaveis de ambiente...")
-if not load_dotenv():
-    raise FileNotFoundError("Não foi possivel carregar as variaveis de ambiente")
-print("Variaveis de Ambiente Carregadas com sucesso!")
-
 from fastapi import FastAPI
 
 app = FastAPI()
