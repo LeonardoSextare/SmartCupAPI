420af876cb597ce319c2f7c50009f23d6ef3d296
Leonardo Sextare <leonardosextare@gmail.com>
2025-04-09 10:04:37 -0300

    melhora database

--- app/database.py
+++ app/database.py
@@ -1,5 +1,5 @@
 from os import getenv
-from typing import Any, Optional, Tuple
+from typing import Any, Literal, Optional, Tuple
 
 import psycopg2 as db
 from psycopg2.extras import RealDictCursor
@@ -13,26 +13,25 @@ def __obter_conexao():
         host=getenv("DB_HOST"),
         port=getenv("DB_PORT"),
     )
-
     return conexao
 
 
-def executar_query(query: str, variaveis: Optional[Tuple[Any, ...]] = None) -> dict[Any, Any] | list[dict[Any, Any]]:
+def executar_query(query: str, variaveis: Optional[Tuple[Any, ...]] = None) -> dict[Any, Any] | list[dict[Any, Any]] | Literal[True]:
     conexao = __obter_conexao()
     try:
-        with conexao, conexao.cursor(cursor_factory=RealDictCursor) as cursor:
+        with conexao.cursor(cursor_factory=RealDictCursor) as cursor:
             cursor.execute(query, variaveis)
+            
+            if cursor.description is None:
+                conexao.commit()
+                return True
+            
             resultados = cursor.fetchall()
+            conexao.commit()
 
             if len(resultados) == 1:
                 return dict(resultados[0])
 
             return [dict(registro) for registro in resultados]
-
     finally:
         conexao.close()
-
-
-if __name__ == "__main__":
-    teste = executar_query("Select * from Administrador")
-    print(teste)
