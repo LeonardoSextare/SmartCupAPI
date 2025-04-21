72095f94d9a23d5b91af28afa045935422a54a99
Leonardo Sextare <leonardosextare@gmail.com>
2025-04-21 17:48:39 -0300

    remove carregamento de variáveis de ambiente desnecessário

--- app/supabase_api.py
+++ app/supabase_api.py
@@ -1,9 +1,5 @@
 from supabase import create_client, Client
 from os import getenv
-from config import carregar_env
-
-carregar_env()
-
 
 url = getenv("SUPABASE_URL")
 key = getenv("SUPABASE_ANON_KEY")
